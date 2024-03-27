0d59892e37e89017f3d5dd1c243331fc8c14007a
M-Abdul-Rehman <aarehman945@gmail.com>
2024-03-27 21:15:14 +0500

    Added a basic navbar as well as some styling

---
+++ un/src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <ul className="opts">
+          <li>about</li>
+          <li>services</li>
+          <li>featured work</li>
+        </ul>
+        <h1 className="nav-logo">Urban Nest</h1>
+        <ul className="opts">
+          <li>Community</li>
+          <li>media</li>
+          <li>contact</li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
