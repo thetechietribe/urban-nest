089fd6e1b358c4acb8bb51fca498c9f499f4cffc
rattle <waqas.khan@thetechietribe.com>
2024-04-16 02:40:16 +0500

    Add About Page

--- un/src/app/about/page.js
+++ un/src/app/about/page.js
@@ -3,6 +3,7 @@ import './styles.css'
 import About1 from '@/components/About1'
 import About2 from '@/components/About2'
 import About3 from '@/components/About3'
+import About4 from '@/components/About4'
 
 export default function AboutPage(){
     
@@ -12,6 +13,7 @@ export default function AboutPage(){
         <About1/>
         <About2/>
         <About3/>
+        <About4/>
         </>
     )
 };
