f1124a62efb450379e9cf9b5d615b89cbc7af5a4
rattle <waqas.khan@thetechietribe.com>
2024-04-17 02:04:19 +0500

    Add Communty Page

--- un/src/app/community/page.js
+++ un/src/app/community/page.js
@@ -1,9 +1,13 @@
+import Com1 from "@/components/Com1";
 import "./styles.css";
 import Navbar from "@/components/Navbar";
+import Com2 from "@/components/Com2";
 export default function CommunityPage() {
   return (
     <>
       <Navbar />
+      <Com1/>
+      <Com2/>
     </>
   );
 }
---
+++ un/src/components/Com2.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Image1 from '../../public/Images/Com1.webp'
+export default function Com2() {
+  return (
+    <div className="c-2">
+        <Image src={Image1} width={721} height={1226}/>
+    </div>
+  )
+}
+
