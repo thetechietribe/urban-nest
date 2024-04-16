8ff38551bd5157248cd1946bf9cc436a0528ca68
rattle <waqas.khan@thetechietribe.com>
2024-04-16 22:27:42 +0500

    Add Services Page

--- un/src/app/services/page.js
+++ un/src/app/services/page.js
@@ -2,6 +2,7 @@ import './styles.css'
 import Navbar from '@/components/Navbar'
 import Service1 from '@/components/Service1'
 import Service2 from '@/components/Service2'
+import Service3 from '@/components/Service3'
 
 export default function ServicePage(){
     return(
@@ -9,6 +10,7 @@ export default function ServicePage(){
         <Navbar/>
         <Service1/>
         <Service2/>
+        <Service3/>
         </>
     )
 };
