8733927415a650f2d4c480e232b80243a270e535
rattle <waqas.khan@thetechietribe.com>
2024-04-16 22:30:34 +0500

    Add Services Page

--- un/src/app/services/page.js
+++ un/src/app/services/page.js
@@ -1,16 +1,22 @@
-import './styles.css'
-import Navbar from '@/components/Navbar'
-import Service1 from '@/components/Service1'
-import Service2 from '@/components/Service2'
-import Service3 from '@/components/Service3'
+import "./styles.css";
+import Navbar from "@/components/Navbar";
+import Service1 from "@/components/Service1";
+import Service2 from "@/components/Service2";
+import Service3 from "@/components/Service3";
+import Join from "@/components/Join";
+import Services from "@/components/Services";
+import Copyright from "@/components/Copyright";
 
-export default function ServicePage(){
-    return(
-        <>
-        <Navbar/>
-        <Service1/>
-        <Service2/>
-        <Service3/>
-        </>
-    )
-};
+export default function ServicePage() {
+  return (
+    <>
+      <Navbar />
+      <Service1 />
+      <Service2 />
+      <Service3 />
+      <Join />
+      <Services />
+      <Copyright />
+    </>
+  );
+}
