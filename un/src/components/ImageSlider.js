8ac758cd7ff2d8ace6dce5e4712c41785c76ffe1
rattle <waqas.khan@thetechietribe.com>
2024-04-19 02:37:09 +0500

    Add Contact Page

--- un/src/components/ImageSlider.js
+++ un/src/components/ImageSlider.js
@@ -1,6 +1,8 @@
 "use client";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import { Autoplay } from "swiper/modules";
+import Glider from "react-glider";
+import "glider-js/glider.min.css";
 import "swiper/css";
 import Image from "next/image";
 import Image1 from "../../public/Images/Image1.webp";
@@ -8,49 +10,44 @@ import Image2 from "../../public/Images/Image2.webp";
 import Image3 from "../../public/Images/Image3.webp";
 import Image4 from "../../public/Images/Image4.webp";
 import Image5 from "../../public/Images/Image5.webp";
+import { Box } from "@mui/material";
 
 export default () => {
+  const images = [Image1, Image2, Image3, Image4, Image5];
+
   return (
-    <Swiper
-      slidesPerView={5}
-      breakpoints={{
-        500: {
-          slidesPerView: 2,
-        },
-        800: {
-          slidesPerView: 3,
-        },
-        1200: {
-          slidesPerView: 4,
-        },
-      }}
-      autoplay={{
-        delay: 2000,
-        disableOnInteraction: false,
-      }}
-      speed={1000}
-      loop={true}
-      spaceBetween={10}
-      modules={[Autoplay]}
-    >
-      <SwiperSlide>
-        <Image src={Image1} height={600} width={450} />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Image src={Image2} height={600} width={450} />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Image src={Image3} height={600} width={450} />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Image src={Image4} height={600} width={450} />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Image src={Image1} height={600} width={450} />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Image src={Image5} height={600} width={450} />
-      </SwiperSlide>
-    </Swiper>
+    // <Box sx={{ width: "100%" }}>
+    //   <Swiper
+    //     breakpoints={{
+    //       500: {
+    //         slidesPerView: 2,
+    //       },
+    //       800: {
+    //         slidesPerView: 3,
+    //       },
+    //       1200: {
+    //         slidesPerView: 4,
+    //       },
+    //     }}
+    //     autoplay={{
+    //       delay: 2000,
+    //       disableOnInteraction: false,
+    //     }}
+    //     loop={true}
+    //     spaceBetween={10}
+    //     modules={[Autoplay]}
+    //   >
+    //     {images.map((image, index) => (
+    //       <SwiperSlide key={index}>
+    //         <Image src={image} height={600} width={450} />
+    //       </SwiperSlide>
+    //     ))}
+    //   </Swiper>
+    // </Box>
+    <Glider draggable  slidesToShow={4} slidesToScroll={1} Autoplay={true} duration={1} style={{"height":"600px"}}>
+      {images.map((image, index) => (
+        <Image src={image} height={600} width={450} style={{"margin-right":"10px","height":"600px"}} />
+      ))}
+    </Glider>
   );
 };
